9373dba209b598c9c47e4c88702c79f4973a3fe8
Joe Thorpe <joethorpe6@protonmail.com>
2021-12-31 13:23:13 +0000

    major cleanup

--- Sources/pjmgmt/main.swift
+++ Sources/pjmgmt/main.swift
@@ -2,21 +2,19 @@ import Foundation
 import ArgumentParser
 import ZIPFoundation
 
-func makeProjectDirectory(projectid: String) -> URL {
-    let projectDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Projects").appendingPathComponent(projectid)
-    
+func makeProjectDirectory(projectid: String, projectPath: URL) {
     let folders = ["Screenshots", "Logs", "Documents", "Reporting"]
     
-    if !FileManager.default.fileExists(atPath: projectDirectoryURL.path) {
+    if !FileManager.default.fileExists(atPath: projectPath.path) {
         do {
-            try FileManager.default.createDirectory(atPath: projectDirectoryURL.path, withIntermediateDirectories: true, attributes: nil)
+            try FileManager.default.createDirectory(atPath: projectPath.path, withIntermediateDirectories: true, attributes: nil)
         } catch {
             print(error.localizedDescription)
         }
     }
     
     for folder in folders {
-        let folderPath = projectDirectoryURL.appendingPathComponent(folder)
+        let folderPath = projectPath.appendingPathComponent(folder)
         if !FileManager.default.fileExists(atPath: folderPath.path) {
             do {
                 try FileManager.default.createDirectory(atPath: folderPath.path, withIntermediateDirectories: true, attributes: nil)
@@ -25,11 +23,10 @@ func makeProjectDirectory(projectid: String) -> URL {
             }
         }
     }
-    return projectDirectoryURL
 }
 
 @available(macOS 10.11, *)
-func generateNotes(projectid: String) {
+func generateNotes(projectid: String, projectPath: URL) {
     let org = """
     #+OPTIONS: num:nil
     #+PROPERTY: header-args :tangle yes :exports both :eval no-export :results output
@@ -48,8 +45,7 @@ func generateNotes(projectid: String) {
         return
     }
 
-    let projectDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Projects").appendingPathComponent(projectid)
-    let notesURL = URL(fileURLWithPath: "notes", relativeTo: projectDirectoryURL).appendingPathExtension("org")
+    let notesURL = URL(fileURLWithPath: "\(projectPath)/notes", relativeTo: projectPath).appendingPathExtension("org")
     
     do {
         try content.write(to: notesURL)
@@ -58,18 +54,18 @@ func generateNotes(projectid: String) {
     }
 }
 
-func archiveProject(projectid: String) {
-    let projectDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Projects").appendingPathComponent(projectid)
+func archiveProject(projectid: String, projectPath: URL) {
+    // there could be a better way to get this
     let projectArchiveURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Projects").appendingPathComponent("\(projectid).zip")
 
     do {
-        try FileManager.default.zipItem(at: projectDirectoryURL, to: projectArchiveURL)
+        try FileManager.default.zipItem(at: projectPath, to: projectArchiveURL)
     } catch {
         print(error.localizedDescription)
     }
 
     do {
-        try FileManager.default.removeItem(at: projectDirectoryURL)
+        try FileManager.default.removeItem(at: projectPath)
     } catch {
         print(error.localizedDescription)
     }
@@ -83,15 +79,17 @@ struct Project: ParsableCommand {
     var archive = false
     
     func run() throws {
-        let _ = makeProjectDirectory(projectid: id)
+        let projectsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Projects/\(id)/")
+        
+        let _ = makeProjectDirectory(projectid: id, projectPath: projectsDirectoryURL)
 
         if #available(macOS 10.11, *) {
-            generateNotes(projectid: id)
+            generateNotes(projectid: id, projectPath: projectsDirectoryURL)
         } else {
             print("it only works in osx 10.11 and newer")
         }
         if archive == true {
-            archiveProject(projectid: id)
+            archiveProject(projectid: id, projectPath: projectsDirectoryURL)
         }
     }
 }
